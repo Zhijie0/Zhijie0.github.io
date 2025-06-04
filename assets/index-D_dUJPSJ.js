b4af0d0ec13d014721cf4f866aacc0046dc2f4b7
xzj <1353753283@qq.com>
2025-06-04 17:26:41 +0800

    up

--- assets/index-D_dUJPSJ.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-8ic76rrg.js";function e(){return r.jsx("div",{className:"practice",children:"123"})}export{e as default};
---
+++ assets/index-DfdnctBO.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-Da0R2DnX.js";function e(){return r.jsx("div",{className:"practice",children:"123"})}export{e as default};
